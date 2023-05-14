192829598f07cdcf987418ea249938ceac40d653
unknown <owenwijaya89@gmail.com>
2023-05-14 23:35:53 +0700

    added styling to the signed in page

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -1,24 +1,69 @@
 import { useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function SignedIn({ joke }) {
   const [revealPunchline, setRevealPunchline] = useState(false);
 
   return (
-    <div>
-      <h1>Signed In</h1>
+    <div className="container">
+      <h1 className="title">Signed In</h1>
 
       {/* Your own presentation of the joke here */}
-      <h2>Joke of the Day:</h2>
-      <p>{joke.setup}</p>
+      <h2 className="joke-title">Joke of the Day:</h2>
+      <p className="joke-setup">{joke.setup}</p>
 
-      {revealPunchline && <p>{joke.punchline}</p>}
+      {revealPunchline && <p className="joke-punchline">{joke.punchline}</p>}
 
-      <button onClick={() => setRevealPunchline(true)}>Reveal Punchline</button>
+      <button className="reveal-button" onClick={() => setRevealPunchline(true)}>
+        Reveal Punchline
+      </button>
+      
+      <style jsx>{`
+        .container {
+          text-align: center;
+        }
 
-      {/* End of your presentation */}
+        .title {
+          font-size: 28px;
+          font-weight: bold;
+          margin-bottom: 20px;
+        }
+
+        .joke-title {
+          font-size: 24px;
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+
+        .joke-setup {
+          font-size: 18px;
+          margin-bottom: 20px;
+        }
+
+        .joke-punchline {
+          font-size: 20px;
+          font-weight: bold;
+          margin-top: 20px;
+        }
+
+        .reveal-button {
+          font-size: 16px;
+          padding: 10px 20px;
+          border-radius: 5px;
+          background-color: #007bff;
+          color: #fff;
+          border: none;
+          cursor: pointer;
+          margin-top: 20px;
+          transition: background-color 0.3s ease;
+        }
+
+        .reveal-button:hover {
+          background-color: #0056b3;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
